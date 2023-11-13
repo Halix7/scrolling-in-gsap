548eb5eaa6b1c8ebc49a024943d3e2b3c5b62c5b
Rohit <halixh39@gmail.com>
2023-11-14 00:28:04 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,16 @@
+gsap.from("#page1 #box",{
+    scale:0,
+    delay:1,
+    duration:1,
+    rotate:360
+})
+gsap.from("#page2 #box",{
+    scale:0,
+    delay:1,
+    duration:1,
+    rotate:360,
+    scrollTrigger:{
+        trigger:"#page2 #box",
+        scroller:"body"
+    }
+})
